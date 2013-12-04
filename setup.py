312e8d1951e3cca9f8c349be9148b4edad33b182
David García Garzón <voki@canvoki.net>
2013-12-04 11:41:10 +0100

    copiat disc dur david

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+
+readme = open("README.md").read()
+
+setup(
+	name = "consolemsg",
+	version = "0.1",
+	description = "Simple semantic functions for colorfull console messages",
+	author = "David Garcia Garzon",
+	author_email = "voki@canvoki.net",
+	url = 'https://github.com/GuifiBaix/python-consolemsg',
+	long_description = readme,
+	license = 'GNU General Public License v3 or later (GPLv3+)',
+	py_modules = [ 'consolemsg' ],
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Topic :: Multimedia',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Intended Audience :: Developers',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Operating System :: OS Independent',
+	],
+)
+
