5fa84ab77787fba7e190532b735e026780e2eb5f
David García Garzón <voki@canvoki.net>
2019-07-26 11:08:50 +0200

    pipes working as-is, new functions out, b, u

--- README.md
+++ README.md
@@ -11,11 +11,12 @@ Instead of `print()` you can use:
 - `error()`
 - `warn()`
 - `success()`
+- `out()`
 
 Also `fail()` prints an error and exits.
 
-All consolemsg functions outputs to `sys.stderr`, so they will
-be separated from your `stdout` when you use piping.
+All consolemsg functions, but `out`, output to `sys.stderr`,
+so they will be separated from your `stdout` when piping.
 
 Extra arguments, will be inserted into the message with format.
 
@@ -23,4 +24,7 @@ For serious logging you should use the `logging` standard module.
 This is a quick and simple solution make the user aware of the
 relevance of the outputs.
 
+Also those functions are Py2 backwards compatible,
+so you can use them instead of regular `print` to make
+your code portable.
 
--- consolemsg.py
+++ consolemsg.py
@@ -1,32 +1,72 @@
 #!/usr/bin/env python
+# -*- encoding: utf8 -*-
+
+def _decoratedStream(stream):
+    # respect the encoding if set
+    if stream.encoding:
+        return stream
+    # if not use utf8
+    import codecs
+    return codecs.getwriter('utf8')(stream)
+
+def stderr():
+    if hasattr(stderr, 'cached'):
+        return stderr.cached
+    import sys
+    stderr.cached = _decoratedStream(sys.stderr)
+    return stderr.cached
+
+def stdout():
+    if hasattr(stdout, 'cached'):
+        return stdout.cached
+    import sys
+    stdout.cached = _decoratedStream(sys.stdout)
+    return stdout.cached
+
+def b(text):
+    if type(text) == type(b''): return text
+    if type(text) == type(u''): return text.encode('utf8')
+    return type(u'')(text).encode('utf8')
+
+def u(text):
+    if type(text) == type(u''): return text
+    if type(text) == type(b''): return text.decode('utf8')
+    return type(u'')(text)
 
 def printStdError(*args) :
-    import sys
-    sys.stderr.write(' '.join(str(arg) for arg in args))
-    sys.stderr.write('\n')
-    sys.stderr.flush()
+    stderr().write(u' '.join(u(arg) for arg in args))
+    stderr().write(u'\n')
+    stderr().flush()
 
-def color(color, message, *args, **kwds) :
+def printStdOut(*args) :
+    stdout().write(u' '.join(u(arg) for arg in args))
+    stdout().write(u'\n')
+    stdout().flush()
+
+def color(color, message, *args, **kwds):
     if args or kwds:
-        message=message.format(*args,**kwds)
-    return "\033[{0}m{1}\033[0m".format(color,message)
+        message=u(message).format(*args,**kwds)
+    return u"\033[{0}m{1}\033[0m".format(u(color),u(message))
 
-def success(message, *args, **kwds) :
-    printStdError(color('32;1', ">> "+message, *args, **kwds))
+def success(message, *args, **kwds):
+    printStdError(color('32;1', ">> "+u(message), *args, **kwds))
 
-def step(message, *args, **kwds) :
-    printStdError(color('34;1', ":: "+message, *args, **kwds))
+def step(message, *args, **kwds):
+    printStdError(color('34;1', ":: "+u(message), *args, **kwds))
 
-def error(message, *args, **kwds) :
-    printStdError(color('31;1', "Error: "+message, *args, **kwds))
+def error(message, *args, **kwds):
+    printStdError(color('31;1', "Error: "+u(message), *args, **kwds))
 
-def warn(message, *args, **kwds) :
-    printStdError(color('33', "Warning: "+message, *args, **kwds))
+def warn(message, *args, **kwds):
+    printStdError(color('33', "Warning: "+u(message), *args, **kwds))
 
-def fail(message, code=-1, *args, **kwds) :
+def fail(message, code=-1, *args, **kwds):
     error(message, *args, **kwds)
     import sys
     sys.exit(code)
+
+def out(message, *args, **kwds):
+    printStdOut(u(message).format(*args,**kwds))
 
 
 if __name__ == "__main__":
@@ -37,7 +77,16 @@ if __name__ == "__main__":
     step('Testing common messages')
     success('{} did this', 'Joe')
     warn('{thing} might be dangerous', thing="Swords")
+    # Parameters
+    error(u'Unicode castaña unicode param {what}', what=u"castañin") # Beyond ASCIII use unicode
+    error(b'Encoded casta\xc3\xb1a unicode param {what}', what=u"castañin") # Some permisivity with templates, not with params
     error('{what} happened', what="Murphy")
+    error(2323) # Message is converted to unicode
+    out('This goes {} to the output', 'undecorated')
+    out(u'Supporting castañas as well')
+    out(b'Supporting encoded casta\xc3\xb1as as well')
     fail('Something very bad happened and i will die. {}', -2, 'Bye')
 
 
+
+# vim: et ts=4 sw=4
